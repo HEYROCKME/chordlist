83f8a302cce9e6636319788d31167781d63cb3e9
Alwin Nedrum <nedrum@gmail.com>
2024-05-23 10:38:08 +0200

    added chord post logic

--- src/chords/chords.controller.ts
+++ src/chords/chords.controller.ts
@@ -1,13 +1,24 @@
 // Adding a new Controller for the chords
 
-import { Controller } from '@nestjs/common';
+// Nest Decorators
+import { Controller, Post, Body } from '@nestjs/common';
+// Service (injectable)
 import { ChordsService } from './chords.service';
 
 @Controller('chords')
 export class ChordsController {
   constructor(private readonly chordsService: ChordsService) {}
   @Post()
-  addChord(): any {
-    this.chordsService.insertChord();
+  addChord(
+    @Body('name') chordName: string,
+    @Body('notes') chordNotes: string,
+    @Body('degree') chordDegree: number,
+  ): any {
+    const generatedID = this.chordsService.insertChord(
+      chordName,
+      chordNotes,
+      chordDegree,
+    );
+    return { id: generatedID };
   }
 }
