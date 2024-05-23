861b56d653a82381c63a0db0c8d5434a37024582
Alwin Nedrum <nedrum@gmail.com>
2024-05-23 20:18:26 +0200

    setup mongoDB with Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChordsModule } from './chords/chords.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ChordsModule], //nest link to ChordsModule
+  imports: [
+    ChordsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_URI'), //Loaded
+      }),
+    }),
+  ], //nest link to ChordsModule
   controllers: [AppController],
   providers: [AppService],
 })
